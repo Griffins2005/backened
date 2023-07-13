64bad118f81058533c42c47ae6a39c8dc5d1e1c6
collinsmathinji <125723113+collinsmathinji@users.noreply.github.com>
2023-07-13 20:50:11 +0300

    noma

---
+++ server/models/game.model.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema(
+  {
+    board: {
+      type: [[String]],
+      required: true,
+      default: [['', '', ''], ['', '', ''], ['', '', '']],
+    },
+    currentPlayer: {
+      type: String,
+      enum: ['X', 'O'],
+      required: true,
+      default: 'X',
+    },
+    status: {
+      type: String,
+      enum: ['In Progress', 'X Wins', 'O Wins', 'Draw'],
+      required: true,
+      default: 'In Progress',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+gameSchema.methods.checkForWin = function () {
+  const board = this.board;
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      this.status = `${board[a]} Wins`;
+      return board[a];
+    }
+  }
+
+  if (board.flat().every((cell) => cell !== '')) {
+    this.status = 'Draw';
+    return 'Draw';
+  }
+
+  return null;
+};
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = Game;
---
+++ server/routes/router.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const Game = require('../models/game.model');
+
+router.post('/', async (req, res) => {
+  try {
+    const newGame = new Game();
+    await newGame.save();
+    res.status(201).json(newGame);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create a new game' });
+  }
+});
+
+router.get('/:id', (req, res) => {
+  Game.findById(req.params.id)
+    .then((game) => res.json(game))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.post('/:id/move', (req, res) => {
+  const { id } = req.params;
+  const { row, col, player } = req.body;
+
+  Game.findById(id)
+    .then((game) => {
+      if (game.status !== 'In Progress') {
+        return res.status(400).json('Game is already over');
+      }
+
+      if (game.currentPlayer !== player) {
+        return res.status(400).json('Invalid player');
+      }
+
+      if (
+        row < 0 ||
+        row > 2 ||
+        col < 0 ||
+        col > 2 ||
+        game.board[row][col] !== ''
+      ) {
+        return res.status(400).json('Invalid move');
+      }
+
+      game.board[row][col] = player;
+      game.currentPlayer = player === 'X' ? 'O' : 'X';
+
+      const winner = game.checkForWin();
+      if (winner) {
+        game.status = winner === 'Draw' ? 'Draw' : `${winner} Wins`;
+      }
+
+      game
+        .save()
+        .then(() => res.json('Move made successfully'))
+        .catch((err) => res.status(400).json('Error: ' + err));
+    })
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
